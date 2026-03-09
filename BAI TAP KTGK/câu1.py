eb8a117fda9b6bbbfd686a151c4c56098a37929a
DoQuoc205 <qdo25044@gmail.com>
2026-03-09 15:01:47 +0700

    Add files via upload

---
+++ BAI TAP KTGK/câu1.py
@@ -0,0 +1,16 @@
+a = input("nhập hệ số a")
+b = input("nhập hệ số b ")
+c = input("nhập hệ số c")
+if a > b and a > c:
+    print("a là số lớn nhất")
+elif a < b and b < c:
+    print("a là số bé nhất")
+if b > a and b > c:
+    print("b là số lớn nhất")
+elif b < a and b < c:
+    print("b là số bé nhất")
+if c > a and c > b:
+    print("c là số lớn nhất")
+elif c < a and c > b:
+    print("c là số bé nhấ4"
+          "t")
